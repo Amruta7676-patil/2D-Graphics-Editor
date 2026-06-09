d14d4a99f998abeefd3a9d8856e4b6d3f63d61c0
Sakshi <sakshicn28@gmail.com>
2026-06-09 15:20:28 +0530

    Initial project version completed

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 #define ROWS 20
 #define COLS 40
@@ -15,14 +15,27 @@ void initCanvas() {
     }
 }
 
-// Display canvas
+// Display canvas (simple clean UI)
 void displayCanvas() {
+    system("cls");
+
+    printf("\n   ===== 2D GRAPHICS EDITOR =====\n\n");
+
+    printf("   ");
+    for(int i=0;i<COLS;i++) printf("--");
+    printf("-\n");
+
     for(int i=0;i<ROWS;i++) {
+        printf("   | ");
         for(int j=0;j<COLS;j++) {
             printf("%c ", canvas[i][j]);
         }
-        printf("\n");
+        printf("|\n");
     }
+
+    printf("   ");
+    for(int i=0;i<COLS;i++) printf("--");
+    printf("-\n");
 }
 
 // Draw Line
@@ -87,13 +100,14 @@ int main() {
     initCanvas();
 
     do {
-        printf("\n--- MENU ---\n");
+        printf("\n========== MENU ==========\n");
         printf("1. Draw Line\n");
         printf("2. Draw Rectangle\n");
         printf("3. Draw Triangle\n");
         printf("4. Draw Circle\n");
         printf("5. Delete Area\n");
         printf("6. Display Canvas\n");
+        printf("7. Modify Object\n");
         printf("0. Exit\n");
 
         printf("Enter choice: ");
@@ -102,32 +116,33 @@ int main() {
         int r,c,w,h,len,rad;
 
         switch(choice) {
+
             case 1:
-                printf("Enter row, col, length: ");
+                printf("Enter row col length: ");
                 scanf("%d %d %d",&r,&c,&len);
                 drawLine(r,c,len);
                 break;
 
             case 2:
-                printf("Enter row, col, width, height: ");
+                printf("Enter row col width height: ");
                 scanf("%d %d %d %d",&r,&c,&w,&h);
                 drawRectangle(r,c,w,h);
                 break;
 
             case 3:
-                printf("Enter row, col, height: ");
+                printf("Enter row col height: ");
                 scanf("%d %d %d",&r,&c,&h);
                 drawTriangle(r,c,h);
                 break;
 
             case 4:
-                printf("Enter center row, col, radius: ");
+                printf("Enter center row col radius: ");
                 scanf("%d %d %d",&r,&c,&rad);
                 drawCircle(r,c,rad);
                 break;
 
             case 5:
-                printf("Enter row, col, width, height: ");
+                printf("Enter row col width height: ");
                 scanf("%d %d %d %d",&r,&c,&w,&h);
                 deleteArea(r,c,w,h);
                 break;
@@ -135,6 +150,46 @@ int main() {
             case 6:
                 displayCanvas();
                 break;
+
+            case 7: {
+                int old_r, old_c, old_w, old_h;
+                int new_choice;
+
+                printf("Enter OLD area: ");
+                scanf("%d %d %d %d", &old_r, &old_c, &old_w, &old_h);
+
+                deleteArea(old_r, old_c, old_w, old_h);
+
+                printf("New object: 1.Line 2.Rect 3.Tri 4.Circle: ");
+                scanf("%d", &new_choice);
+
+                switch(new_choice) {
+                    case 1:
+                        printf("Enter row col length: ");
+                        scanf("%d %d %d",&r,&c,&len);
+                        drawLine(r,c,len);
+                        break;
+
+                    case 2:
+                        printf("Enter row col width height: ");
+                        scanf("%d %d %d %d",&r,&c,&w,&h);
+                        drawRectangle(r,c,w,h);
+                        break;
+
+                    case 3:
+                        printf("Enter row col height: ");
+                        scanf("%d %d %d",&r,&c,&h);
+                        drawTriangle(r,c,h);
+                        break;
+
+                    case 4:
+                        printf("Enter center row col radius: ");
+                        scanf("%d %d %d",&r,&c,&rad);
+                        drawCircle(r,c,rad);
+                        break;
+                }
+                break;
+            }
         }
 
     } while(choice != 0);
